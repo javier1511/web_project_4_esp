9092817f7c3e6c9c7dd7ca9ed2242fa34c30b374
javier270987 <javier_castillo270987@hotmail.com>
2022-12-20 18:52:52 -0600

    revisionhoracio

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-let popupContainer = document.querySelector(".popup__container");
-let openPopupButton  = document.querySelector(".profile__edit-button");
-let popup = document.querySelector(".popup");
-let overlay = document.querySelector(".overlay")
-let closeButton = document.querySelector(".popup__close-icon");
-let popupDisplay =  document.querySelector(".popup__opened");
+let overlay = document.querySelector(".overlay");
+
+let popupDisplay = document.querySelector(".popup__opened");
 let profileTitle = document.querySelector(".profile__title");
-let profileJob =  document.querySelector(".profile__subtitle");
+let profileJob = document.querySelector(".profile__subtitle");
 let inputName = document.querySelector("#name");
 let inputJob = document.querySelector("#job");
+let popupContainer = document.querySelector(".popup__container");
+let popup = document.querySelector(".popup");
+let openPopupButton = document.querySelector(".profile__edit-button");
+let closeButton = document.querySelector(".popup__close-icon");
+let saveButton = document.querySelector("#save__button");
 inputName.value = profileTitle.textContent;
 inputJob.value = profileJob.textContent;
 
-
-
-
-function toggle () {
-popup.classList.toggle("popup__opened");
-overlay.classList.toggle("overlay__visible");
+function toggle() {
+  popup.classList.toggle("popup__opened");
+  overlay.classList.toggle("overlay__visible");
 }
 
 openPopupButton.addEventListener("click", toggle);
 closeButton.addEventListener("click", toggle);
 
-
-function save () {
-    popup.classList.toggle("popup__opened");
-overlay.classList.toggle("overlay__visible");
-profileTitle.textContent = inputName.value
-profileJob.textContent = inputJob.value 
+function saveForm() {
+  popup.classList.toggle("popup__opened");
+  overlay.classList.toggle("overlay__visible");
 }
+
+saveButton.addEventListener("click", saveForm);
+
+function submitForm(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+}
+saveButton.addEventListener("click", submitForm);
